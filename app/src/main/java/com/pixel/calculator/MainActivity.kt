417c2643f7e78e2d3ccbc45a0f2d8d6c27d847ed
blvckgsm-ui <blvckgsm@gmail.com>
2026-06-27 22:18:03 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/pixel/calculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.pixel.calculator
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import com.pixel.calculator.ui.CalculatorScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                CalculatorScreen()
+            }
+        }
+    }
+}
